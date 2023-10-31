e53774e86893e62aba35a0e38ab617e6458b9071
kotleni <yavarenikya@gmail.com>
2023-10-31 20:51:09 +0200

    fix: can't scroll icons selector

--- shared/src/commonMain/kotlin/app/kotleni/pomodoro/ui/main/IconSelector.kt
+++ shared/src/commonMain/kotlin/app/kotleni/pomodoro/ui/main/IconSelector.kt
@@ -1,15 +1,17 @@
 package app.kotleni.pomodoro.ui.main
 
+import PlatformType
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -22,42 +24,40 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
+import getPatformType
 
 @Composable
 fun IconSelector(icons: List<ImageVector>, onSelected: (index: Int, icon: ImageVector) -> Unit) {
     var selectedIndex by remember { mutableIntStateOf(0) }
 
-    LazyRow(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 0.dp, top = 8.dp, bottom = 8.dp, end = 0.dp)
-    ) {
-        item { Spacer(modifier = Modifier.width(8.dp)) }
-        items(icons.size) { index ->
-            Box(
-                modifier = Modifier
-                    .padding(8.dp)
-                    .clickable {
-                        selectedIndex = index
-                        onSelected(index, icons[index])
-                    }
-            ) {
-                Box(
-                    modifier = Modifier
-                        .clip(RoundedCornerShape(8.dp))
-                        .background(if (selectedIndex == index) MaterialTheme.colorScheme.primaryContainer else MaterialTheme.colorScheme.secondaryContainer)
-                ) {
-                    Image(
-                        modifier = Modifier
-                            .padding(16.dp),
-                        imageVector = icons[index],
-                        contentDescription = "",
-                        colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onPrimaryContainer)
-                    )
-                }
+    if(getPatformType() == PlatformType.DESKTOP) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(5),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 0.dp, top = 8.dp, bottom = 8.dp, end = 0.dp)
+        ) {
+            items(icons.size) { index ->
+                IconSelectorItem(icons[index], selectedIndex == index, onSelected = {
+                    selectedIndex = index
+                    onSelected(index, icons[index])
+                })
             }
         }
-
-        item { Spacer(modifier = Modifier.width(8.dp)) }
+    } else {
+        LazyRow (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 0.dp, top = 8.dp, bottom = 8.dp, end = 0.dp)
+        ) {
+            item { Spacer(modifier = Modifier.width(8.dp)) }
+            items(icons.size) { index ->
+                IconSelectorItem(icons[index], selectedIndex == index, onSelected = {
+                    selectedIndex = index
+                    onSelected(index, icons[index])
+                })
+            }
+            item { Spacer(modifier = Modifier.width(8.dp)) }
+        }
     }
 }
---
+++ shared/src/commonMain/kotlin/app/kotleni/pomodoro/ui/main/IconSelectorItem.kt
@@ -0,0 +1,40 @@
+package app.kotleni.pomodoro.ui.main
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun IconSelectorItem(icon: ImageVector, isSelected: Boolean, onSelected: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable {
+                onSelected()
+            }
+    ) {
+        Box(
+            modifier = Modifier
+                .clip(RoundedCornerShape(8.dp))
+                .background(if (isSelected) MaterialTheme.colorScheme.primaryContainer else MaterialTheme.colorScheme.secondaryContainer)
+        ) {
+            Image(
+                modifier = Modifier
+                    .padding(16.dp),
+                imageVector = icon,
+                contentDescription = "",
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onPrimaryContainer)
+            )
+        }
+    }
+}
