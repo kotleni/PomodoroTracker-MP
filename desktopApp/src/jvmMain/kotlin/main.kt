95c3a74605c1f5049da5fc9ef62429d6d36a8aed
kotleni <yavarenikya@gmail.com>
2023-11-14 16:12:06 +0200

    fix: window start size and position adjusted

--- desktopApp/src/jvmMain/kotlin/main.kt
+++ desktopApp/src/jvmMain/kotlin/main.kt
@@ -1,6 +1,11 @@
 
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import app.kotleni.pomodoro.di.koinModules
 import org.koin.core.context.startKoin
 
@@ -9,9 +14,15 @@ fun main() = application {
         modules(koinModules)
     }
 
+    val state = rememberWindowState(
+        size = DpSize(420.dp, 700.dp),
+        position = WindowPosition(Alignment.Center)
+    )
+
     Window(
         onCloseRequest = ::exitApplication,
-        title = "PomodoroTracker"
+        title = "PomodoroTracker",
+        state = state
     ) {
         MainView()
     }
